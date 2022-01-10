df49ca85d518b12b4d3740d21738d835826b4462
vigneshshettyin <vigneshshetty.in@gmail.com>
2022-01-10 07:31:50 +0530

    Initial Commit

---
+++ Coursesity/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Coursesity
+//
+//  Created by Vignesh Shetty on 10/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Coursesity/CoursesityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoursesityApp.swift
+//  Coursesity
+//
+//  Created by Vignesh Shetty on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CoursesityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
